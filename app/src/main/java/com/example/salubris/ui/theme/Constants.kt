ed5bf6d47540df3ecd776f4ef9a63c5f80afd973
n0escape200 <n0_escape200@yahoo.com>
2026-02-10 18:20:52 +0200

    worked on the home page

--- app/src/main/java/com/example/salubris/ui/theme/Constants.kt
+++ app/src/main/java/com/example/salubris/ui/theme/Constants.kt
@@ -7,14 +7,3 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.unit.dp
 
 val MainContainerBorder = RoundedCornerShape(10.dp)
-
-object FooterItems{
-    val Home = {
-        val label = "Home"
-        val icon = Icons.Default.Home
-    }
-    val Tracking = {
-        val label = "Tracking"
-        val icon = Icons.Default.Create
-    }
-}
