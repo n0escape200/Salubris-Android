1744c7e2438d4c1d184b979f54db33018712262c
n0escape200 <n0_escape200@yahoo.com>
2026-02-11 21:32:33 +0200

    working on the tracking page

--- app/src/main/java/com/example/salubris/ui/components/PageModal.kt
+++ app/src/main/java/com/example/salubris/ui/components/PageModal.kt
@@ -9,16 +9,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.salubris.ui.theme.ContainerBackground
 
+
 @Composable
 fun PageModal(tabs: Map<String, @Composable () -> Unit>) {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(10.dp)) {
+
+    var selectedTab by remember { mutableStateOf(tabs.keys.first()) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -26,20 +38,26 @@ fun PageModal(tabs: Map<String, @Composable () -> Unit>) {
                 .padding(5.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            tabs.forEach { (key, _) ->
+            tabs.forEach { (title, _) ->
                 Box(
                     modifier = Modifier
-                        .background(Color(40, 40, 40), shape = RoundedCornerShape(5.dp))
+                        .background(
+                            if (selectedTab == title) Color(70, 70, 70) else Color(40, 40, 40),
+                            shape = RoundedCornerShape(5.dp)
+                        )
                         .padding(5.dp)
                 ) {
-                    Text(key, color = Color.White)
+                    Button(
+                        onClick = { selectedTab = title },
+                    ) {
+                        Text(title, color = Color.White)
+                    }
                 }
             }
         }
+
         Column {
-            tabs.forEach { (_, content) ->
-                content()
-            }
+            tabs[selectedTab]?.invoke()
         }
     }
 }
--- app/src/main/java/com/example/salubris/ui/screens/pages/Tracking.kt
+++ app/src/main/java/com/example/salubris/ui/screens/pages/Tracking.kt
@@ -5,16 +5,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.example.salubris.ui.components.PageModal
 import com.example.salubris.ui.screens.subpages.Macros
+import com.example.salubris.ui.screens.subpages.Steps
+import com.example.salubris.ui.screens.subpages.Water
 
 
 val tabs = mapOf<String, @Composable () -> Unit>(
     "Macros" to { Macros() },
+    "Water" to { Water() },
+    "Steps" to { Steps() }
 )
 
 
 @Composable
 fun Tracking(navController: NavController) {
-    Column(){
+    Column() {
         PageModal(tabs)
     }
 }
